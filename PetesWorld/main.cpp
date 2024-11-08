221b43045e6deced1a17f2953efbd622370e6861
(Student  C00309098) Nikola Prusac <C00309098@setu.ie>
2024-11-08 10:35:59 +0000

    Letter D added

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -22,7 +22,7 @@ int main()
 		std::cout << "You need to select a number between 1 and 5" << std::endl;
 		return 0;
 	}
-	std::cout << "Pick a letter from A to C or X to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to D or X to Z" << std::endl; // change this line each iteration
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
 	{
@@ -103,7 +103,31 @@ int main()
 			std::cout << "Cayman islands, Cook island, Cuba" << std::endl;
 		}
 	}
-	
+	if (letter == 'd' || letter == 'D')
+	{
+		if (region == 1)
+		{
+			std::cout << "there is no Ds in the Americas" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "Denmark" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Djibouti" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There is no Ds in Asia" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Dominica, Dominican Republic" << std::endl;
+		}
+	}
+
+
 	if (letter == 'x' || letter == 'X')
 	{
 		if (region == 1)
