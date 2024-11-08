a920fb2ca41515c3251b5382b7c71027b206b849
(Student  C00309342) Jake Reid <C00309342@setu.ie>
2024-11-08 10:08:35 +0000

    Letter X added

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -22,7 +22,7 @@ int main()
 		std::cout << "You need to select a number between 1 and 5" << std::endl;
 		return 0;
 	}
-	std::cout << "Pick a letter from A to B or Y to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to B or X to Z" << std::endl; // change this line each iteration
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
 	{
@@ -77,6 +77,31 @@ int main()
 		if (region == 5)
 		{
 			std::cout << "Bahamas, bahrain Arabian, Barbados" << std::endl;
+		}
+	}
+
+
+	if (letter == 'x' || letter == 'X')
+	{
+		if (region == 1)
+		{
+			std::cout << "There are no Xs in the Americas" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Xs in Europe" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "There are no Xs in Africa" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no Xs in Asia" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "There are no Xs in the rest of the world" << std::endl;
 		}
 	}
 	if (letter == 'y' || letter == 'Y')
